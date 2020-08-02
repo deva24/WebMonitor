85f80cad95dd6cc37573a1a119abc794c8485012
Devarshi Hazarika <49090172+deva24@users.noreply.github.com>
2020-08-02 19:55:53 +0530

    notify failed attempts, observe vfs slots

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const Diff = require('text-diff');
 const puppeteer = require('puppeteer');
+const nodeNotifier = require('node-notifier');
 const { notify } = require('node-notifier');
 
-notify('begin observer');
+nodeNotifier.notify('begin observer');
 
 
 if (!fs.existsSync('tmp'))
@@ -15,6 +16,9 @@ if (!fs.existsSync('tmp'))
 let scheduler = (function ()
 {
     let filename = path.resolve('tmp', 'schedule.json');
+    let firstFailed = null;
+    let lastFailed = null;
+    let failCooldown = 2;
 
     function getLastRun()
     {
@@ -51,12 +55,27 @@ let scheduler = (function ()
 
     function hasRan()
     {
+        firstFailed = null;
+        lastFailed = null;
+        failCooldown = 2;
         let obj = getNow();
         fs.writeFileSync(filename, JSON.stringify(obj));
     }
 
     function shouldRun()
     {
+        let nowRun = getNow();
+
+        if (lastFailed !== null)
+        {
+            let nowMin = nowRun.h * 60 + nowRun.m;
+            if (nowMin < lastFailed)
+                nowMin += 24 * 60;
+
+            if (nowMin - lastFailed < failCooldown)
+                return false;
+        }
+
         let lastRun = getLastRun();
         if (lastRun === null)
         {
@@ -67,7 +86,6 @@ let scheduler = (function ()
             lastRun.h += 0.5
             lastRun.m -= 30;
         }
-        let nowRun = getNow();
         if (nowRun.m >= 30)
         {
             nowRun.h += 0.5;
@@ -92,7 +110,30 @@ let scheduler = (function ()
         });
     }
 
-    return { getLastRun, getNow, hasRan, shouldRun, sleep }
+    function hasNotRun()
+    {
+        let now = getNow();
+        let nowMin = now.h * 60 + now.m;
+
+        if (firstFailed !== null)
+        {
+            failCooldown *= 2;
+            if (failCooldown > 60) failCooldown = 60;
+
+            if (nowMin < firstFailed) nowMin += 24 * 60;
+            let diffMin = nowMin - firstFailed;
+
+            if (diffMin > failCooldown)
+                nodeNotifier.notify('observer process failed');
+        }
+        else
+        {
+            firstFailed = nowMin;
+        }
+        lastFailed = nowMin;
+    }
+
+    return { getLastRun, getNow, hasRan, shouldRun, sleep, hasNotRun }
 })();
 
 let observer = (function ()
@@ -211,7 +252,7 @@ let fetcher = (function ()
                 '--ignore-certifcate-errors-spki-list',
                 '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
             ];
-        
+
             const browser = await puppeteer.launch({
                 args,
                 headless: true,
@@ -247,11 +288,21 @@ let fetcher = (function ()
                 return false;
             }
 
+            console.log('fetch slots');
+            let txt4 = await getSlots(browser);
+
+            if (txt4 === null)
+            {
+                console.log('failed fetching german embassy');
+                return false;
+            }
+
             console.log("fetch successful, observing changes");
 
             let changed1 = await observer.notice(txt1, 'vfs-global');
             let changed2 = await observer.notice(txt2, 'vfs-german');
             let changed3 = await observer.notice(txt3, 'german-embassy');
+            let changed4 = await observer.notice(txt4, 'german-vfs-slot');
 
             if (changed1)
             {
@@ -272,6 +323,13 @@ let fetcher = (function ()
                 notify('german embassy changed', () =>
                 {
                     require('child_process').exec('start ' + path.resolve('tmp', 'german-embassy', 'temp-diff.html'));
+                });
+            }
+            if (changed4)
+            {
+                notify('german vfs slot', () =>
+                {
+                    require('child_process').exec('start ' + path.resolve('tmp', 'german-vfs-slot', 'temp-diff.html'));
                 });
             }
             await browser.close();
@@ -353,7 +411,161 @@ let fetcher = (function ()
         return null;
     }
 
+    async function getSlots(browser)
+    {
+        let ret = '';
+        try
+        {
+
+            const page = await browser.newPage();
+            await page.goto('https://visa.vfsglobal.com/ind/en/deu/application-detail');
+            let usernameSelector = '#Email';
+            let passwordSelector = '#Password';
+            let submitBtnSelector = '#btnLogin';
+
+            console.log("Loggin in");
+            await scheduler.sleep(1000);
+            await page.type(usernameSelector, 'devarshi.hazarika@live.in');
+            await scheduler.sleep(1000);
+            await page.type(passwordSelector, 'BA9S53nZzp*iL3M');
+            await scheduler.sleep(1000);
+            await page.click(submitBtnSelector);
+
+            await page.waitForNavigation();
+            await page.goto('https://visa.vfsglobal.com/ind/en/deu/application-detail');
+
+            console.log('Select center and visa type');
+            let centerSelector = '#VisaApplicationCenterddl';
+
+            await page.waitForSelector(centerSelector);
+            await scheduler.sleep(5000);
+
+            await page.select(centerSelector, 'DEL');
+
+
+
+            let timeoutSecs = 60;
+            let countUp = 0;
+
+            while (true)
+            {
+                let visaType1Selector = '#VisaTypeddl';
+                let visaType1List = await page.$$eval(visaType1Selector + '>option', arr => arr.map(it => ({ val: it.value, txt: it.innerHTML })));
+
+                if (countUp++ >= timeoutSecs)
+                {
+                    throw 'Visa Type 1 didnt load';
+                }
+
+                if (visaType1List.length > 1)
+                {
+                    let visaType1OptionText = 'National Visa - More than 90 days';
+                    ret += `Visa Type 1:\n` + visaType1List.map(x => x.txt).join('\n') + '\n';
+
+
+                    if (visaType1List.map(x => x.txt).indexOf(visaType1OptionText) >= 0)
+                    {
+                        let visaType1Value = visaType1List.filter(x => x.txt == visaType1OptionText)[0].val;
+                        await page.select(visaType1Selector, visaType1Value);
+                    }
+                    else
+                    {
+                        return ret;
+                    }
+
+                    break;
+                }
+
+                await scheduler.sleep(1000);
+            }
+
+            countUp = 0;
+            while (true)
+            {
+                let visaType2Selector = '#SubVisaCategoryOptions'
+                let visaType2List = await page.$$eval(visaType2Selector + '>option', arr => arr.map(it => ({ val: it.value, txt: it.innerHTML })));
+
+                if (countUp++ >= timeoutSecs)
+                {
+                    throw 'Visa Type 1 didnt load';
+                }
+
+                if (visaType2List.length > 1)
+                {
+                    await scheduler.sleep(5000);
+                    let visaType2OptionText = 'Contract holders for highly qualified employment holding a binding approval letter (ZAV-Zustimmung) and Dependents';
+                    ret += `Visa Type 2:\n` + visaType2List.map(x => x.txt).join('\n') + '\n';
+
+                    if (visaType2List.map(x => x.txt).indexOf(visaType2OptionText) >= 0)
+                    {
+                        let visaType2Value = visaType2List.filter(x => x.txt == visaType2OptionText)[0].val
+                        await page.select(visaType2Selector, visaType2Value);
+                    }
+                    else
+                    {
+                        return ret;
+                    }
+
+                    break;
+                }
+
+                await scheduler.sleep(1000);
+            }
+
+            console.log('Continue to form');
+            await page.click('#btnApplicationDetailContinue');
+
+            await page.waitForNavigation();
+
+            console.log("Fill the form");
+            await page.type('#forename_', 'Devarshi');
+            await page.type('#lastName_', 'Hazarika');
+            await page.select('#gender_', 'Male');
+            await page.select('#nationality_', 'IND');
+            await page.type('#dob_', '24/01/1992');
+            await page.type('#passnum_', 'P644754');
+            await page.type('#pexpirydate_', '21/02/2027');
+            await page.type('#countryCode_', '91');
+            await page.type('#phoneNumber_', '7042732281');
+            await page.type('#Email_', 'devarshi.hazarika@live.in');
+
+            console.log("Save form");
+            await page.click('#btnSave');
+
+            // #wait
+            console.log("Continue to slot selection");
+            await page.waitForSelector('#btnContinue');
+            await scheduler.sleep(5000);
+
+            await page.click('#btnContinue');
+            await scheduler.sleep(5000);
+            console.log("Noticing slots for this month");
+
+            let legendsEle = await page.$$eval('#legendData li', els => els.map(el => el.textContent));
+            let legendsMonth0 = legendsEle.map(x => x.trim()).join(',');
+            ret += '\nMonth 0:\n' + legendsMonth0 + '\n\n';
+
+            console.log("Go next month");
+            await page.waitForSelector('[title="Next Month"]');
+            await page.click('[title="Next Month"]');
+
+            console.log("Noticing slots for next month");
+            await scheduler.sleep(5000);
+            legendsEle = await page.$$eval('#legendData li', els => els.map(el => el.textContent));
+            let legendsMonth1 = legendsEle.map(x => x.trim()).join(',');
+            ret += 'Month 1:\n' + legendsMonth1 + '\n\n';
+            await page.close();
+
+        } catch (error)
+        {
+            debugger;
+            console.error(error);
+        }
+        return ret;
+    }
+
     return { runner };
+
 
 })();
 
@@ -379,14 +591,19 @@ setInterval(async () =>
     if (isRunning)
         return;
 
-    if (scheduler.shouldRun())
+    let shouldRun = (scheduler.shouldRun())
+    if (shouldRun)
     {
         isRunning = true;
         let didRun = await fetcher.runner();
-
         if (didRun)
         {
             scheduler.hasRan();
+        }
+        else
+        {
+            scheduler.hasNotRun();
+            console.log('failed');
         }
         isRunning = false;
     }
